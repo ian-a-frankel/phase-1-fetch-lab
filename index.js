49f6df4e5b3fdb26d5fb6932b05da755af691625
Ian Frankel <ian.frankel@gmail.com>
2023-10-05 22:57:20 -0400

    fetched books

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
+  const gameOfThronesDB = fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(books => renderBooks(books))
+
+  return gameOfThronesDB
+
 }
 
 function renderBooks(books) {
+  //console.log(books)
   const main = document.querySelector('main');
   books.forEach(book => {
+    //console.log(book)
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
